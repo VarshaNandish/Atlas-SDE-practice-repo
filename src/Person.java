2e9bae3a1999799174fadb07a9746414f2e759e1
Varsha <spvarsha6@gmail.com>
2025-06-04 19:41:04 +0530

    Adding today's Java programs practiced on Atlas training

---
+++ src/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    private String name; // 'name' is declared as private
+
+    // Getter method to read 'name'
+    public String getName() {
+        return name;
+    }
+
+    // Setter method to modify 'name'
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
---
+++ src/Task17.java
@@ -0,0 +1,13 @@
+public class Task17 {
+
+    public static void main(String[] args) {
+
+        Person myObj = new Person();
+
+        myObj.setName("John");
+
+        System.out.println(myObj.getName());
+
+    }
+}
+
