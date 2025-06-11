f28498668d27ace8e6d27cbc9334303041496c5e
Varsha SP <168334975+VarshaNandish@users.noreply.github.com>
2025-06-11 23:10:36 +0530

    Delete src directory

--- src/Task05.java
+++
@@ -1,9 +0,0 @@
-public class Task05 {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
-
-        String result = (a > b) ? "a is greater than b" : "a is not greater than b";
-        System.out.println(result);
-    }
-}
